8a4ef86bc3fec950e95b0b340e11d14274df665b
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2023-05-27 23:22:16 +0200

    Refactoring user Store

--- app/stores/user.ts
+++ app/stores/user.ts
@@ -2,104 +2,59 @@ import { Kind } from "nostr-tools";
 import { create } from "zustand";
 import { POOL, RELAYS } from "../lib/constants";
 
+// This is the structure found on the "content" property of a nostr event after parsed to JSON
 export type User = {
-  about: string
-  display_name: string
-  lud06: string
-  name: string
-  nip05: string
-  picture: string
-  website: string
-  pubkey: string
+  about: string;
+  display_name: string;
+  lud06: string;
+  name: string;
+  nip05: string;
+  picture: string;
+  website: string;
 };
 
-
-export interface UserState extends User {
-  setUser: (user: User) => void;
-  setPubkey: (pubkey: string) => void;
+export interface Profile extends User {
+  relay: string;
 }
 
-export const userStore = create<UserState>((set) => ({
-  about: "",
-  display_name: "",
-  lud06: "",
-  name: "",
-  nip05: "",
-  picture: "",
-  website: "",
+export type UserStore = {
+  profiles: Profile[];
+  pubkey: string;
+  // This will return an array of profiles for each of the selected relays (only if the user has a
+  // profile defined in that relay)
+  login: (pubkey: string) => void;
+};
+
+export const userStore = create<UserStore>((set) => ({
+  profiles: [],
   pubkey: "",
-  setUser: (user: User) => {
-    return set({
-      about: user.about,
-      display_name: user.display_name,
-      lud06: user.lud06,
-      name: user.name,
-      nip05: user.nip05,
-      picture: user.picture,
-      website: user.website,
-    });
-  },
-  setPubkey: (pubkey: string) => {
-    return set({
-      pubkey: pubkey,
-    });
+  login: async (pubkey) => {
+    const profiles = await getProfiles(pubkey);
+    set({ profiles, pubkey });
   },
 }));
 
-export type User2 = {
-  about: string
-  display_name: string
-  lud06: string
-  name: string
-  nip05: string
-  picture: string
-  website: string
-  pubkey: string
-  getUser: (pubkey: string) => void
-};
-
-export const userStore2 = create<User2>((set) => ({
-  about: "",
-  display_name: "",
-  lud06: "",
-  name: "",
-  nip05: "",
-  picture: "",
-  website: "",
-  pubkey: "",
-  getUser: (pubkey: string) => {
-    login(pubkey, set)
-  }
-}))
-
-// TODO:
-// refactor this to get instead of a subscription
-// rename to fetch profile instead of login
-function login(pubkey: string, callback: (user: User) => void) {
-
-  const sub = POOL.sub(RELAYS, [
+async function getProfiles(pubkey: string): Promise<Profile[]> {
+  // Fetch all profiles per relay
+  const listRes = await POOL.list(RELAYS, [
     {
-      kinds: [Kind.Metadata],
       authors: [pubkey],
+      kinds: [Kind.Metadata],
     },
-  ])
+  ]);
+  console.log("listRes: ", listRes);
 
-  // Depending on the design decion of hackrnews this should only be of length 1
-  console.log(RELAYS)
-  let profile: User[] = new Array<User>()
+  const profiles: Profile[] = listRes.map((e) => {
+    let profile: User = JSON.parse(e.content);
+    console.log("profile: ", profile);
+    const relayOfOrigin = POOL.seenOn(e.id)[0];
 
-  sub.on("event", (event) => {
-    profile.push(JSON.parse(event.content))
-  })
+    return {
+      ...profile,
+      relay: relayOfOrigin,
+    };
+  });
 
-  sub.on("eose", () => {
-    console.log(profile)
-    if (profile.length > 0) {
-      callback({ ...profile[0], pubkey })
-      console.log("Profile: ", profile)
-    } else {
-      console.log("no profile")
-    }
-    sub.unsub()
-  })
+  console.log("json profiles: ", profiles);
+  return profiles;
 }
